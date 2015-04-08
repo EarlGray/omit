ffc566b8722c5a2d0c6918de3dd6ac58121c7423
Dmytro S <dmytrish@gmail.com>
2015-04-09 00:26:07 +0300

    pack files parsing works

--- Main.hs
+++ Main.hs
@@ -8,6 +8,7 @@ import Data.Word
 import Data.Bits
 import qualified Data.List as L
 import qualified Data.Map as M
+import qualified Data.Set as S
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.UTF8 as BU
@@ -18,12 +19,15 @@ import qualified Data.Digest.Pure.SHA as SHA
 import System.IO
 import System.Time
 import System.Directory
+import System.Posix (fileSize, getFileStatus)
+import System.Posix.Types
 import System.Environment (getArgs)
 import System.Console.ANSI as TTY
 import System.Exit (exitFailure, exitSuccess)
 import Numeric (readHex)
 import Text.Printf
 
+bool thenb elseb cond = if cond then thenb else elseb
 maybeOr msg = maybe (error msg) id
 splitBy num lst = L.unfoldr (\s -> if null s then Nothing else Just $ splitAt num s) lst
 
@@ -33,33 +37,31 @@ type SHAHash = B.ByteString
 showSHA = concatMap (printf "%02x" )
 readSHA = B.pack . map (fst . head . readHex) . splitBy 2
 
+type HashInfoMap = (M.Map SHAHash (Int, Int, Word32)) -- (packOffset, packSize, crc32)
+
 objpathFor hash = concat ["/objects/", take 2 hash, "/", drop 2 hash]
 changeFileExtension ext = reverse . (reverse ext ++) . tail . dropWhile (/= '.') . reverse
 
+getBlob :: FilePath -> [(FilePath, M.Map SHAHash (Int, Int, Word32))] -> String
+           -> IO (String {-type-}, Int {-len-}, BL.ByteString {-blob-})
 getBlob gitdir idxmaps hash = do
     isobj <- doesFileExist (gitdir ++ objpathFor hash)
     if isobj then parseBlob <$> Zlib.decompress <$> BL.readFile (gitdir ++ objpathFor hash)
     else let (idxfile, idxmap) = head $ filter (((readSHA hash) `M.member`) . snd) idxmaps
              packfile = (gitdir ++ "/objects/pack/" ++ changeFileExtension ".pack" idxfile)
-             getblobinfo inf = do
-                w <- getWord8
-                let inf' = (inf `shiftL` 7) .|. (fromIntegral w .&. 0x3f)
-                if w `testBit` 7 then getblobinfo inf' else return inf'
-             getblob = do
-                skip $ fst $ fromJust $ M.lookup (readSHA hash) idxmap
-                inf <- getblobinfo (0 :: Int)
-                let blobty = inf .&. 3
-                let bloblen = inf `shiftR` 3
-                blob <- BL.fromStrict <$> getByteString bloblen
-                return (inf, bloblen, blobty, blob)
+             skipblobinfo (t, n) = getWord8 >>= ((bool (skipblobinfo (t, n+1)) (return (t, n))) . (`testBit` 7))
+             blobinfo = getWord8 >>= (\w -> (if w `testBit` 7 then skipblobinfo else return) (w, 1))
+             getblob blobpos blobsz = do
+                skip blobpos
+                (ty, skipped) <- blobinfo
+                zblob <- getByteString (blobsz - skipped)
+                return (ty, BL.fromStrict zblob) 
          in do
-                putStrLn $ "offset in pack = " ++ show (M.lookup (readSHA hash) idxmap)
-                (inf, bloblen, blobty, blob) <- runGet getblob <$> BL.readFile packfile
-                putStrLn $ "inf = " ++ printf "0x%x" inf
-                putStrLn $ "blobtype = " ++ show blobty
-                putStrLn $ "bloblen  = " ++ show bloblen
-                error "TODO"
-            
+             let Just (blobpos, blobsz, _) = M.lookup (readSHA hash) idxmap
+             (ty, zblob) <- runGet (getblob blobpos blobsz) <$> BL.readFile packfile
+             let blob = Zlib.decompress zblob
+             let Just blobty = L.lookup (ty .&. 0x70) [(0x10,"commit"), (0x20,"tree"), (0x30,"blob")] 
+             return (blobty, fromIntegral $ BL.length blob, blob)
 
 parseBlob :: BL.ByteString -> (String, Int, BL.ByteString) -- blobtype, bloblen, blob
 parseBlob str = let (btype, tl') = BL.break (== 0x20) str ; (slen, tl) = BL.break (== 0) tl'
@@ -75,7 +77,7 @@ parseTreeObject = L.unfoldr parseEntry . BL.unpack -- [(mode::String, len::Strin
 prettyTreeObject :: [(String, String, String)] -> String
 prettyTreeObject = unlines . map (\(mode, path, hash) -> concat [mode, " blob ", hash, "    ", path])
 
-getIdxFile_v2 :: Get (M.Map SHAHash (Int, Word32)) -- M.Map SHAHash (Offset, CRC32)
+getIdxFile_v2 :: Get (M.Map SHAHash (Int, Word32))
 getIdxFile_v2 = do
     indv <- replicateM 0x100 getWord32be
     let lastind = fromIntegral $ last indv
@@ -85,12 +87,15 @@ getIdxFile_v2 = do
     -- TODO: 8b offsets
     return $ M.fromAscList $ zip hashv $ zip offv crc32v
 
+parseIdxFile_v2 :: FilePath -> IO HashInfoMap -- (offset, size, crc32)
 parseIdxFile_v2 idxfile = do
-    idxdata <- BL.readFile idxfile 
+    idxdata <- BL.readFile idxfile
+    packlen <- fromIntegral <$> fileSize <$> getFileStatus (changeFileExtension ".pack" idxfile)
     let (idxbody, trail) = BL.splitAt (BL.length idxdata - 20) idxdata
     when ((show $ SHA.sha1 idxbody) /= (showSHA $ BL.unpack trail)) $ error "idxfile: idx hash invalid"
-    let (0xff744f63, 2, idxmap) = runGet (liftM3 (,,) getWord32be getWord32be getIdxFile_v2) idxbody
-    return idxmap 
+    let (0xff744f63, 2, idxmap') = runGet (liftM3 (,,) getWord32be getWord32be getIdxFile_v2) idxbody
+    let offs' = S.fromList $ ((map fst $ M.elems idxmap') ++ [packlen - 20])
+    return $ M.map (\(off, crc32) -> (off, (fromJust $ S.lookupGT off offs') - off, crc32)) idxmap'
 
 main = do
     argv <- getArgs
@@ -122,10 +127,10 @@ main = do
       ("verify-pack":argv') -> do
         let (verbose, packfile) = ("-v" `elem` argv', last argv')
         let verifyPack = do
-            offmap <- parseIdxFile_v2 $ changeFileExtension ".idx" packfile
-            let printHash (hsh, (off, crc32)) = putStrLn $ showSHA (B.unpack hsh) ++ "  " ++ show off
-            when verbose $ forM_ (M.toList offmap) printHash
-            offmap `seq` exitSuccess
+                offmap <- parseIdxFile_v2 $ changeFileExtension ".idx" packfile
+                let printHash (hsh, (off, sz, crc32)) = putStrLn $ L.intercalate " " [showSHA (B.unpack hsh), show sz, show off]
+                when verbose $ forM_ (M.toList offmap) printHash
+                offmap `seq` return ()
         verifyPack `Exc.catch` (\(e :: Exc.SomeException) -> when verbose (hPrint stderr e) >> exitFailure)
 
       ("log":[]) -> do
