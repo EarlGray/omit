f6a5c09e2d4f915ffed67cffa6fd2fc31e2541dd
Dmytro S <dmytrish@gmail.com>
2015-04-09 21:31:57 +0300

    compilable `omit diff` : does not work yet

--- Main.hs
+++ Main.hs
@@ -7,18 +7,20 @@ import Data.Char
 import Data.Word
 import Data.Bits
 import qualified Data.List as L
-import qualified Data.Map as M
 import qualified Data.Set as S
+import qualified Data.Map as M
+import qualified Data.IntMap as IM
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.ByteString.UTF8 as BU
 import qualified Data.ByteString.Lazy.UTF8 as BLU
 import Data.Binary.Get as BinGet
+import Data.Algorithm.Diff as Diff
 import Codec.Compression.Zlib as Zlib
 import qualified Data.Digest.Pure.SHA as SHA
 import System.IO
 import System.Time
-import System.Directory
+import System.Directory as Dir
 import System.Posix (fileSize, getFileStatus)
 import System.Posix.Types
 import System.Environment (getArgs)
@@ -38,9 +40,15 @@ showSHA = concatMap (printf "%02x" )
 readSHA = B.pack . map (fst . head . readHex) . splitBy 2
 
 type HashInfoMap = (M.Map SHAHash (Int, Int, Word32)) -- (packOffset, packSize, crc32)
+data IndexEntry = IndexEntry { indCTime::ClockTime, indMTime::ClockTime, indDev::Word32, indIno::Word32,
+        indMode::Word32, indUID::Word32, indGID::Word32, indFileSize::Word32, indSHA::SHAHash,
+        indFl::Word16, indFName::B.ByteString }
 
 objpathFor hash = concat ["/objects/", take 2 hash, "/", drop 2 hash]
 changeFileExtension ext = reverse . (reverse ext ++) . tail . dropWhile (/= '.') . reverse
+
+blobify :: String -> BL.ByteString -> BL.ByteString
+blobify blobty objdata = BL.append (BLU.fromString (blobty ++ " " ++ show (BL.length objdata) ++ "\0")) objdata
 
 getBlob :: FilePath -> [(FilePath, M.Map SHAHash (Int, Int, Word32))] -> String
            -> IO (String {-type-}, Int {-len-}, BL.ByteString {-blob-})
@@ -55,19 +63,19 @@ getBlob gitdir idxmaps hash = do
                 skip blobpos
                 (ty, skipped) <- blobinfo
                 zblob <- getByteString (blobsz - skipped)
-                return (ty, BL.fromStrict zblob) 
+                return (ty, BL.fromStrict zblob)
          in do
              let Just (blobpos, blobsz, _) = M.lookup (readSHA hash) idxmap
              (ty, zblob) <- runGet (getblob blobpos blobsz) <$> BL.readFile packfile
              let blob = Zlib.decompress zblob
-             let Just blobty = L.lookup (ty .&. 0x70) [(0x10,"commit"), (0x20,"tree"), (0x30,"blob")] 
+             let Just blobty = L.lookup (ty .&. 0x70) [(0x10,"commit"), (0x20,"tree"), (0x30,"blob")]
              return (blobty, fromIntegral $ BL.length blob, blob)
 
 parseBlob :: BL.ByteString -> (String, Int, BL.ByteString) -- blobtype, bloblen, blob
 parseBlob str = let (btype, tl') = BL.break (== 0x20) str ; (slen, tl) = BL.break (== 0) tl'
                 in (BLU.toString btype, read $ BLU.toString slen, BL.tail tl)
 
-parseTreeObject :: BL.ByteString -> [(String, String, String)] 
+parseTreeObject :: BL.ByteString -> [(String, String, String)]
 parseTreeObject = L.unfoldr parseEntry . BL.unpack -- [(mode::String, len::String, path::String)]
   where parseEntry [] = Nothing
         parseEntry bl = let (hd, (_:tl)) = splitAt (fromJust $ L.findIndex (== 0) bl) bl in
@@ -97,8 +105,8 @@ parseIdxFile_v2 idxfile = do
     let offs' = S.fromList $ ((map fst $ M.elems idxmap') ++ [packlen - 20])
     return $ M.map (\(off, crc32) -> (off, (fromJust $ S.lookupGT off offs') - off, crc32)) idxmap'
 
-parseIndex :: BL.ByteString -> [B.ByteString]
-parseIndex dat = map (\([ctsec, ctusec, mtsec, mtusec, stdev, stino, stmode, stuid, stgid, fsize], sha, flags, fname) -> fname) idxdata
+parseIndex :: BL.ByteString -> [IndexEntry]
+parseIndex dat = map makeIdxentry idxdata
     -- read extensions
     -- verify SHA
   where
@@ -106,6 +114,27 @@ parseIndex dat = map (\([ctsec, ctusec, mtsec, mtusec, stdev, stino, stmode, stu
     go nb bs = (B.break (== 0) <$> getByteString nb) >>= (\(d, z) -> (if B.null z then go 8 else return)(B.append bs d))
     getIdxEntry = liftM4 (,,,) (replicateM 10 getWord32be) (getByteString 20) getWord16be (go 2 B.empty)
     idxdata = runGet (replicateM (fromIntegral nentries) getIdxEntry) (BL.drop 12 dat)
+    makeIdxentry ([ctsec, ctusec, mtsec, mtusec, stdev, stino, stmode, stuid, stgid, fsize], sha, flags, fname) =
+      IndexEntry (TOD (fromIntegral ctsec) (fromIntegral ctusec)) (TOD (fromIntegral mtsec) (fromIntegral mtusec))
+                 stdev stino stmode stuid stgid fsize sha flags fname
+
+groupByAscRange :: [(Int, a)] -> [[a]]
+groupByAscRange = reverse . map reverse . snd . foldr go (0, [[]])
+  where go (k, v) (n, grps) | k == n + 1 = (k, (v : head grps) : tail grps)
+                            | otherwise = (k, if null (head grps) then grps else []:grps)
+
+notFirst diffval = case diffval of { First _ -> False; _ -> True }
+notSecond diffval = case diffval of { Second _ -> False; _ -> True }
+isBoth diffval = case diffval of { Both _ _ -> True; _ -> False }
+
+contextDiff :: Eq t => Int -> [Diff t] -> [[Diff (Int, t)]]
+contextDiff nctx diff = groupByAscRange $ IM.toAscList ctxmap
+  where annot (Both ln1 ln2) (num1, num2, res) = (succ num1, succ num2, Both (num1,ln1) (num2,ln2) : res)
+        annot (First ln)     (num1, num2, res) = (succ num1, num2,      First (num1, ln) : res)
+        annot (Second ln)    (num1, num2, res) = (num1,      succ num2, Second (num2, ln) : res)
+        lnmap = IM.fromList $ zip [1..] $ reverse $ (\(_,_,e) -> e) $ foldr annot (1,1,[]) diff
+        isInContext num = not $ all isBoth $ catMaybes [ IM.lookup i lnmap | i <- [(num - nctx)..(num + nctx)] ]
+        ctxmap = IM.foldWithKey (\n dv res -> if isInContext n then IM.insert n dv res else res) IM.empty lnmap
 
 main = do
     argv <- getArgs
@@ -118,6 +147,9 @@ main = do
     let parents = map ((\d -> "/"++d++"/.git") . L.intercalate "/") . takeWhile (not.null) . iterate init $ cpath
     pardirsexist <- mapM (\d -> (,d) <$> doesDirectoryExist d) parents
     let gitdir = maybe (error ".git directory not found") snd . listToMaybe . filter fst $ pardirsexist
+    let workdir = changeFileExtension "" gitdir
+
+    index <- parseIndex <$> BL.readFile (gitdir ++ "/index")
 
     -- find pack files and load them
     idxfiles <- filter (L.isSuffixOf ".idx") <$> getDirectoryContents (gitdir ++ "/objects/pack")
@@ -128,7 +160,7 @@ main = do
         (blobtype, bloblen, blob) <- getBlob gitdir idxmaps hash
         putStr $ maybe (error "Usage: omit cat-file [-t|-s|-p] <hash>") id $ lookup opt
           [("-t", blobtype ++ "\n"), ("-s", show bloblen ++ "\n"),
-           ("-p", maybe (error "bad file") id $ lookup blobtype 
+           ("-p", maybe (error "bad file") id $ lookup blobtype
               [("blob", BLU.toString blob), ("commit", BLU.toString blob),
                ("tree", prettyTreeObject $ parseTreeObject blob)]),
            ("blob", BLU.toString blob), ("tree", prettyTreeObject $ parseTreeObject blob),
@@ -138,7 +170,8 @@ main = do
         let (verbose, packfile) = ("-v" `elem` argv', last argv')
         let verifyPack = do
                 offmap <- parseIdxFile_v2 $ changeFileExtension ".idx" packfile
-                let printHash (hsh, (off, sz, crc32)) = putStrLn $ L.intercalate " " [showSHA (B.unpack hsh), show sz, show off]
+                let printHash (hsh, (off, sz, crc32)) =
+                        putStrLn $ L.intercalate " " [showSHA (B.unpack hsh), show sz, show off]
                 when verbose $ forM_ (M.toList offmap) printHash
                 offmap `seq` return ()
         verifyPack `Exc.catch` (\(e :: Exc.SomeException) -> when verbose (hPrint stderr e) >> exitFailure)
@@ -148,7 +181,8 @@ main = do
         let printCommit commit = do
                 ("commit", _, blob) <- getBlob gitdir idxmaps commit
                 let (commMeta, commMsg) = break null $ lines $ BLU.toString blob
-                let (cmTZ : cmEpoch : cmAuthor) = reverse $ maybeOr "No commit author" $ commitHeader "author" commMeta
+                let (cmTZ : cmEpoch : cmAuthor) =
+                        reverse $ maybeOr "No commit author" $ commitHeader "author" commMeta
                 colPutStrLn Yellow $ "commit " ++ commit
                 putStrLn $ "Author:\t" ++ unwords (drop 1 . reverse $ cmAuthor)
                 putStrLn $ "Date\t" ++ show (TOD (read cmEpoch) 0)
@@ -156,11 +190,37 @@ main = do
                 putStrLn ""
                 let cmPar = commitHeader "parent" commMeta
                 when (isJust cmPar) $ let ["parent", parent] = fromJust cmPar in printCommit parent
-                
+
         ("ref", (':':' ':path)) <- (break (== ':') . head . lines) <$> readFile (gitdir ++ "/HEAD")
         commit <- head <$> lines <$> readFile (gitdir ++ "/" ++ path)
         printCommit commit
 
-      ["ls-files"] -> parseIndex <$> BL.readFile (gitdir ++ "/index") >>= mapM_ (putStrLn . BU.toString)
+      ("ls-files":argv') -> mapM_ (putStrLn . BU.toString . indFName) index
 
-      _ -> error "Usage: omit [cat-file|verify-pack|log]"
+      ("diff":argv') -> do
+        case argv' of
+          [] -> forM_ index $ \ie -> do
+                    let (fname, stageSHA) = (BU.toString (indFName ie), (showSHA $ B.unpack $ indSHA ie))
+                    workdirBlob <- BL.readFile (workdir ++ "/" ++ fname)
+                    let workSHA = show (SHA.sha1 $ blobify "blob" workdirBlob)
+                    when (workSHA /= stageSHA) $ do
+                      putStrLn $ concat ["### fname=", fname, ", sha=", workSHA, ", stage=", stageSHA]
+                      let workdirLines = map BLU.toString $ BLU.lines workdirBlob
+                      ("blob", _, stagedBlob) <- getBlob gitdir idxmaps stageSHA
+                      let stagedLines = map BLU.toString $ BLU.lines stagedBlob
+                          printCtx [] = []
+                          printCtx grp@((Both (n1,_) (n2,ln)):grp') =
+                              let (len1, len2) = (length $ filter notSecond grp, length $ filter notFirst grp)
+                                  prettygrp = map (\dv -> case dv of
+                                                      Both (_,ln) (_,_) -> " " ++ ln
+                                                      First (_, ln) -> "-" ++ ln
+                                                      Second (_, ln) -> "+" ++ ln) grp
+                              in ((printf "@@ -%d,%d +%d,%d @@ " n1 len1 n2 len2) ++ head prettygrp):(tail prettygrp)
+                          diffcap = [ printf "diff --git a/%s b/%s" fname fname,
+                                      "index ", printf "--- a/%s\n+++ b/%s" fname fname ]
+                          prettyDiff df = {- diffcap ++ -} (concat $ map printCtx $ contextDiff 4 df)
+                      mapM_ putStrLn $ prettyDiff $ Diff.getDiff workdirLines stagedLines
+
+          _ -> hPutStrLn stderr $ "Usage: omit diff"
+
+      _ -> error "Usage: omit [cat-file|verify-pack|ls-files|log]"
